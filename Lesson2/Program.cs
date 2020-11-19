a22eac141e9741fceed82a9d3c68e7f3d5f18482
Adeyemi Olayemi <Triumphantsam30@gmail.com>
2020-11-19 23:56:50 +0100

    Add project files.

---
+++ Lesson2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var i = 20;
+            Console.WriteLine("i = {0}", i);
+
+            i = i + 1;
+            Console.WriteLine("i = {0}", i);
+
+            i += 2;
+            Console.WriteLine("i = {0}", i);
+            Console.WriteLine("i = {0}.", --i);
+            Console.WriteLine("i = {0}, but now is {1} again.", i--, i);
+        }
+    }
+}
